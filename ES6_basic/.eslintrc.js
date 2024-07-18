a68f5cba88329597adf1b2f3b8161c237d9f821b
psychohight <7298@holbertonstudents.com>
2024-07-18 18:56:07 +0200

    12

--- ES6_basic/.eslintrc.js
+++ ES6_basic/.eslintrc.js
@@ -1,45 +1,35 @@
 module.exports = {
-    env: {
-      browser: true,
-      es6: true,
-      node: true,
-      jest: true
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-      "eslint:recommended",
-      "plugin:react/recommended"
-    ],
-    globals: {
-      Atomics: "readonly",
-      SharedArrayBuffer: "readonly"
-    },
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true
-      },
-      ecmaVersion: 2018,
-      sourceType: "module"
-    },
-    plugins: [
-      "react"
-    ],
-    rules: {
-      "indent": [
-        "error",
-        2
-      ],
-      "linebreak-style": [
-        "error",
-        "unix"
-      ],
-      "quotes": [
-        "error",
-        "single"
-      ],
-      "semi": [
-        "error",
-        "always"
-      ]
-    }
-  };
-  
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-trailing-spaces': 'error',
+    'no-multiple-empty-lines': ['error', { max: 0 }],
+    'eol-last': ['error', 'always'],
+  },
+};
