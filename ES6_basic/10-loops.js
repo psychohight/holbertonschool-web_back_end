700ee825d19eeb7a622ce64478eee13a6cb4e695
psychohight <7298@holbertonstudents.com>
2024-07-18 18:15:05 +0200

    3 a 12

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+    for (let i = 0; i < array.length; i++) {
+      array[i] = appendString + array[i];
+    }
+  
+    return array;
+  }
+  
