7ee484a2e21178b7377cf5b8d208df61457ac666
psychohight <7298@holbertonstudents.com>
2024-07-21 21:08:20 +0200

    5 a 10

--- ES6_classes/.eslintrc.js
+++ ES6_classes/.eslintrc.js
@@ -1,41 +1,44 @@
-module.exports = {
-    env: {
-      browser: true,
-      es6: true,
-      node: true,
-      jest: true,
-    },
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-    ],
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
-    },
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
+const { FlatCompat } = require('@eslint/eslintrc');
+const { configs: eslintRecommended } = require('@eslint/js');
+const jestRecommended = require('eslint-plugin-jest').configs.recommended;
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: eslintRecommended.recommended,
+});
+
+module.exports = [
+  ...compat.extends('eslint:recommended'),
+  ...compat.extends('plugin:jest/recommended'),
+  {
+    files: ['**/*.js'],
+    languageOptions: {
       ecmaVersion: 2018,
       sourceType: 'module',
-    },
-    plugins: [
-      'react',
-    ],
-    settings: {
-      react: {
-        version: 'detect',
+      globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+        browser: true,
+        es2021: true,
+        jest: true,
       },
     },
-    rules: {
-      indent: ['error', 2],
-      'linebreak-style': ['error', 'unix'],
-      quotes: ['error', 'single'],
-      semi: ['error', 'always'],
-      'no-trailing-spaces': 'error',
-      'no-multiple-empty-lines': ['error', { max: 0 }],
-      'eol-last': ['error', 'always'],
+    plugins: {
+      jest: require('eslint-plugin-jest'),
     },
-  };
-  
+    rules: {
+      'no-console': 'off',
+      'no-shadow': 'off',
+      'no-restricted-syntax': [
+        'error',
+        'LabeledStatement',
+        'WithStatement',
+      ],
+    },
+    settings: {
+      jest: {
+        version: 26,
+      },
+    },
+  },
+];
