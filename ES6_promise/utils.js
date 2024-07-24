0ee244a543063992fe02afd5a47eb6bd2fc4f64d
psychohight <7298@holbertonstudents.com>
2024-07-24 14:47:35 +0200

    0 a 4

---
+++ ES6_promise/utils.js
@@ -0,0 +1,8 @@
+export function uploadPhoto() {
+    return Promise.resolve({ body: 'photo-profile-1' });
+  }
+  
+  export function createUser() {
+    return Promise.resolve({ firstName: 'Guillaume', lastName: 'Salva' });
+  }
+  
