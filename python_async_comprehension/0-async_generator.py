1ba1e6bcc2c3945cca4b925e88f463abc6d6e298
psychohight <enzo.tenis74@gmail.com>
2024-08-19 15:38:19 +0200

    style

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
-"""0. Async Generator"""
+"""
+0. Async Generator
+"""
 
 import asyncio
 import random
 
 async def async_generator():
+    """ Coroutine that generates 10 random numbers """
     for _ in range(10):
         await asyncio.sleep(1)  # Attend 1 seconde de manière asynchrone
         yield random.uniform(0, 10)  # Génère un nombre aléatoire entre 0 et 10
