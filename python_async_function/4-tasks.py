ea62b3e36a1dcc43aee8079bfe59115c663b4951
psychohight <enzo.tenis74@gmail.com>
2024-08-19 16:50:00 +0200

    style

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -10,7 +10,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-   """ Returns list of delays in ascending """
-   tasks = [task_wait_random(max_delay) for _ in range(n)]
-   delays = [await task for task in tasks]
-   return sorted(delays)
+    """ Returns list of delays in ascending """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = [await task for task in tasks]
+    return sorted(delays)
