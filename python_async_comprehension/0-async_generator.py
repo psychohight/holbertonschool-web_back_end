e911c4294bb485759829f1734652df9468707812
psychohight <enzo.tenis74@gmail.com>
2024-08-19 15:26:21 +0200

    0 + base

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""0. Async Generator"""
+
+import asyncio
+import random
+
+async def async_generator():
+    for _ in range(10):
+        await asyncio.sleep(1)  # Attend 1 seconde de manière asynchrone
+        yield random.uniform(0, 10)  # Génère un nombre aléatoire entre 0 et 10
