23cef4db1a62453dd326d8ee3c1c4eebbdaf10f8
psychohight <enzo.tenis74@gmail.com>
2024-08-22 17:21:16 +0200

    2 a 4

--- NoSQL/3-all.js
+++ NoSQL/3-all.js
@@ -1,3 +1,3 @@
-// Ce script liste tous les documents dans la collection 'school'
+// Ce script liste tous les documents dans la collection school
 db = db.getSiblingDB("my_db"); // Remplacer "my_db" par le nom de la base de données si différent
 db.school.find().forEach(doc => printjson(doc));
--- NoSQL/4-match.js
+++ NoSQL/4-match.js
@@ -1,3 +1,3 @@
-// Ce script liste tous les documents avec name="Holberton school" dans la collection 'school'
+// Ce script liste tous les documents avec name= Holberton school dans la collection school
 db = db.getSiblingDB("my_db"); // Remplacer "my_db" par le nom de la base de données si différent
 db.school.find({ name: "Holberton school" }).forEach(doc => printjson(doc));
