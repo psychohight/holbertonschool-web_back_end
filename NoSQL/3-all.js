ff8a5cebef6a04ab7b368e4c1c0fad8eca688ea3
psychohight <enzo.tenis74@gmail.com>
2024-08-22 17:12:54 +0200

    1 a 4

--- NoSQL/3-all.js
+++ NoSQL/3-all.js
@@ -1,3 +1,3 @@
-#!/usr/bin/env python3
+// Ce script liste tous les documents dans la collection 'school'
 db = db.getSiblingDB("my_db"); // Remplacer "my_db" par le nom de la base de données si différent
 db.school.find().forEach(doc => printjson(doc));
